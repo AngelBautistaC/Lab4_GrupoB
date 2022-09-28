e2bf5dda58860d3cd40c5976bbd99c89bad9b10d
caleb <caleb.casapaico@pucp.edu.pe>
2022-09-27 21:58:33 -0500

    Listo mascota 95% alsdkaslkdjas

---
+++ src/main/java/com/example/lab4_20222/dto/ServiciosMascotaDto.java
@@ -0,0 +1,14 @@
+package com.example.lab4_20222.dto;
+
+import java.util.Date;
+
+public interface ServiciosMascotaDto {
+
+    String getNombremascota();
+    Integer getEdad();
+    Date getHorainicio();
+    Integer getDuracion();
+    String getEntrega();
+    String getResponsable();
+    String getServicio();
+}
