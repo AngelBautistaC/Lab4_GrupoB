4ecdc33bc6f40fe2facabcb983fa6cb56d52637b
FrankLaquise <a20175947@pucp.edu.pe>
2022-09-27 20:46:06 -0500

    entityes

---
+++ src/main/java/com/example/lab4_20222/entity/Mascota.java
@@ -0,0 +1,116 @@
+package com.example.lab4_20222.entity;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="mascota")
+public class Mascota{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, name = "idmascota")
+    private int idmascota;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "anho")
+    private String anho;
+
+    @Column(name = "historia")
+    private String historia;
+
+    @Column(name = "observaciones")
+    private String observaciones;
+
+    @Column(name = "sexo")
+    private String sexo;
+    @Column(name = "raza_otros")
+    private String raza_otros;
+
+
+
+    @ManyToOne
+    @JoinColumn(name = "raza_especie_idraza")
+    private Raza_especie raza_especie;
+
+
+    @ManyToOne
+    @JoinColumn(name = "cuenta_idcuenta")
+    private Cuenta cuenta_idcuenta;
+
+
+
+    public int getIdmascota() {
+        return idmascota;
+    }
+
+    public void setIdmascota(int idmascota) {
+        this.idmascota = idmascota;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAnho() {
+        return anho;
+    }
+
+    public void setAnho(String anho) {
+        this.anho = anho;
+    }
+
+    public String getHistoria() {
+        return historia;
+    }
+
+    public void setHistoria(String historia) {
+        this.historia = historia;
+    }
+
+    public String getObservaciones() {
+        return observaciones;
+    }
+
+    public void setObservaciones(String observaciones) {
+        this.observaciones = observaciones;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getRaza_otros() {
+        return raza_otros;
+    }
+
+    public void setRaza_otros(String raza_otros) {
+        this.raza_otros = raza_otros;
+    }
+
+    public Raza_especie getRaza_especie() {
+        return raza_especie;
+    }
+
+    public void setRaza_especie(Raza_especie raza_especie) {
+        this.raza_especie = raza_especie;
+    }
+
+    public Cuenta getCuenta_idcuenta() {
+        return cuenta_idcuenta;
+    }
+
+    public void setCuenta_idcuenta(Cuenta cuenta_idcuenta) {
+        this.cuenta_idcuenta = cuenta_idcuenta;
+    }
+}
+
