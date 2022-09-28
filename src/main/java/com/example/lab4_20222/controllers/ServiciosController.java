50c18fe3bbb07352fbfc3210824dbb3662b9ed89
Carlos1507 <ayala.carlos@pucp.edu.pe>
2022-09-27 21:08:26 -0500

    guardar serv

--- src/main/java/com/example/lab4_20222/controllers/ServiciosController.java
+++ src/main/java/com/example/lab4_20222/controllers/ServiciosController.java
@@ -1,8 +1,33 @@
 package com.example.lab4_20222.controllers;
 
+import com.example.lab4_20222.repositories.CuentaRepository;
+import com.example.lab4_20222.repositories.MascotaRepository;
+import com.example.lab4_20222.repositories.OpcionRepository;
+import com.example.lab4_20222.repositories.ResponsableRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping(value = "/servicio")
 public class ServiciosController {
+    @Autowired
+    OpcionRepository opcionRepository;
+    @Autowired
+    MascotaRepository mascotaRepository;
+    @Autowired
+    CuentaRepository cuentaRepository;
+    @Autowired
+    ResponsableRepository responsableRepository;
 
+    @GetMapping(value = "/guardarserv")
+    public String crearServ(Model model){
+        model.addAttribute("listaOpcion", opcionRepository.findAll());
+        model.addAttribute("listaMascotas", mascotaRepository.findAll());
+        model.addAttribute("listaCuenta",cuentaRepository.findAll());
+        model.addAttribute("listaResponsable", responsableRepository.findAll());
+        return "servicio_nuevo";
+    }
 }
