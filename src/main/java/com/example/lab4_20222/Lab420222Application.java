5f4155a878e65c2087882172cbd4cefa332026ae
Angel Alvaro Bautista Cutipa <a20196044@pucp.edu.pe>
2022-09-27 19:32:13 -0500

    1st push :'D

---
+++ src/main/java/com/example/lab4_20222/Lab420222Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20222;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420222Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420222Application.class, args);
+    }
+
+}
