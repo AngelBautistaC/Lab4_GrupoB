da58f474384098dce41d2da15ba48822af1ad05b
Stefhanie Jaramillo <a20190674@pucp.edu.pe>
2022-09-27 21:10:51 -0500

    html fino

--- src/main/java/com/example/lab4_20222/controllers/ServiciosController.java
+++ src/main/java/com/example/lab4_20222/controllers/ServiciosController.java
@@ -1,14 +1,15 @@
 package com.example.lab4_20222.controllers;
 
-import com.example.lab4_20222.repositories.CuentaRepository;
-import com.example.lab4_20222.repositories.MascotaRepository;
-import com.example.lab4_20222.repositories.OpcionRepository;
-import com.example.lab4_20222.repositories.ResponsableRepository;
+import com.example.lab4_20222.entity.Servicio;
+import com.example.lab4_20222.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 
 @Controller
 @RequestMapping(value = "/servicio")
@@ -21,6 +22,17 @@ public class ServiciosController {
     CuentaRepository cuentaRepository;
     @Autowired
     ResponsableRepository responsableRepository;
+    @Autowired
+    ServicioRepository servicioRepository;
+
+    @GetMapping(value = {"/lista", ""})
+    public String listarServicios(Model model) {
+        List<Servicio> lista = servicioRepository.findAll();
+        model.addAttribute("servicioLista", lista);
+        model.addAttribute("listaOpServ", opcionRepository.findAll());
+        return "servicio/lista";
+    }
+
 
     @GetMapping(value = "/guardarserv")
     public String crearServ(Model model){
