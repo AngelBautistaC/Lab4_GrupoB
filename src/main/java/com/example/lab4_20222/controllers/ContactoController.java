a5b8550eefa916abda8e7b5f445cd0ea5bb52074
FrankLaquise <a20175947@pucp.edu.pe>
2022-09-27 21:13:44 -0500

    contacto controller

--- src/main/java/com/example/lab4_20222/controllers/ContactoController.java
+++ src/main/java/com/example/lab4_20222/controllers/ContactoController.java
@@ -1,11 +1,16 @@
 package com.example.lab4_20222.controllers;
 
+import com.example.lab4_20222.entity.Cuenta;
 import com.example.lab4_20222.repositories.CuentaRepository;
 import com.example.lab4_20222.repositories.MascotaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class ContactoController {
@@ -16,9 +21,35 @@ public class ContactoController {
     CuentaRepository cuentaRepository;
 
     @GetMapping(value = {"", "/lista"})
-    public String listaContacto(Model model){
+    public String listaContacto(Model model) {
         model.addAttribute("listaContacto", cuentaRepository.findAll());
         return "contacto/lista";
+    }
+
+
+
+
+    @GetMapping("/new")
+    public String nuevoContactoForm(Model model) {
+        //model.addAttribute("listaContacto", cuentaRepository.findAll());
+        return "contacto/newForm";
+    }
+
+
+    @GetMapping("/editar")
+    public String editarContacto(@ModelAttribute("contacto") Cuenta contacto,
+                                 @RequestParam("id") int id,
+                                 Model model) {
+        Optional<Cuenta> optcontacto = cuentaRepository.findById(id);
+        System.out.println(id);
+        if (optcontacto.isPresent()) {
+            contacto = optcontacto.get();
+            model.addAttribute("contacto", contacto);
+            model.addAttribute("listaCuenta", cuentaRepository.findAll());
+            return "/contacto/editar";
+        } else {
+            return "redirect:/contacto/lista";
+        }
     }
 
 
